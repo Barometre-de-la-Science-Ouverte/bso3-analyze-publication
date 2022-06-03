7e9bc45f18bfae5308ed07c9ecec61be61485ad5
eric <eric.jeangirard@recherche.gouv.fr>
2022-06-03 17:09:24 +0200

    elastic

--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -4,13 +4,122 @@ import os
 import requests
 import pandas as pd
 
+from urllib import parse
 from project.server.main.utils_swift import download_container, upload_object
+from project.server.main.elastic import reset_index
 from project.server.main.logger import get_logger
 
 logger = get_logger(__name__)
 
 volume = '/data'
 container = 'bso3_publications_dump'
+
+FRENCH_ALPHA2 = ['fr', 'gp', 'gf', 'mq', 're', 'yt', 'pm', 'mf', 'bl', 'wf', 'tf', 'nc', 'pf']
+
+ES_LOGIN_BSO_BACK = os.getenv('ES_LOGIN_BSO_BACK', '')
+ES_PASSWORD_BSO_BACK = os.getenv('ES_PASSWORD_BSO_BACK', '')
+ES_URL = os.getenv('ES_URL', 'http://localhost:9200')
+
+def import_es(args):
+    index_name = 'bso-datacite'
+    enriched_output_file = '/data/datacite_fr.jsonl'
+    # elastic
+    es_url_without_http = ES_URL.replace('https://','').replace('http://','')
+    es_host = f'https://{ES_LOGIN_BSO_BACK}:{parse.quote(ES_PASSWORD_BSO_BACK)}@{es_url_without_http}'
+    logger.debug('loading datacite index')
+    reset_index(index=index_name)
+    elasticimport = f"elasticdump --input={enriched_output_file} --output={es_host}{index_name} --type=data --limit 50 " + "--transform='doc._source=Object.assign({},doc)'"
+    logger.debug(f'{elasticimport}')
+    logger.debug('starting import in elastic')
+    os.system(elasticimport)
+
+def create_task_download(args):
+    dump_file = args.get('dump_file', 'datacite_dump_20211022')
+    CHUNK_SIZE = 128
+    DATACITE_DUMP_URL = f'https://archive.org/download/{dump_file}/{dump_file}.json.zst'
+    response = requests.get(url=DATACITE_DUMP_URL, stream=True)
+    filename = DATACITE_DUMP_URL.split('/')[-1]
+    datacite_downloaded_file = f'{volume}/dump/{filename}'
+    os.system(f'mkdir -p {volume}/dump')
+    with open(file=datacite_downloaded_file, mode='wb') as file:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            file.write(chunk)
+    cmd = f'cd {volume}/dump && unzstd {dump_file}.json.zst'
+    os.system(cmd)
+    cmd = f'cd {volume}/dump && split -l 1000000 {dump_file}.json'
+    os.system(cmd)
+
+def create_task_split(nb_line):
+    dump_file = 'datacite_dump_20211022'
+    cmd = f'cd {volume}/dump && rm -rf x* && split -a 3 -l {nb_line} {dump_file}.json'
+    os.system(cmd)
+
+def create_task_tmp(filename):
+    elements = pd.read_json(filename, lines=True).to_dict(orient='records')
+    affiliations_cache = {}
+    for elt in elements:
+        for c in elt.get('attributes', {}).get('contributors', []) + elt.get('attributes', {}).get('creators', []):
+            for aff in c.get('affiliation', []):
+                if aff not in affiliations_cache:
+                    affiliations_cache[aff] = {'nb': 0, 'dois':[]}
+                affiliations_cache[aff]['nb'] += 1
+    logger.debug(f'{len(elements)} elements, {len(affiliations_cache)} affiliations')
+    for aff in affiliations_cache:
+        res = requests.post('http://matcher-affiliation:5001/match_api', json={"query": aff, "type": 'country'}).json()
+        if res['results']:
+            affiliations_cache[aff]['detected_countries'] = res['results']
+    fr_elements = []
+    for elt in elements:
+        detected_countries = []
+        is_fr = False
+        for c in elt.get('attributes', {}).get('contributors', []) + elt.get('attributes', {}).get('creators', []):
+            for aff in c.get('affiliation', []):
+                if aff in affiliations_cache:
+                    detected_countries += affiliations_cache[aff].get('detected_countries', [])
+        detected_countries = list(set(detected_countries))
+        elt['detected_countries'] = detected_countries
+        for x in detected_countries:
+            if x in FRENCH_ALPHA2:
+                is_fr = True
+        if is_fr:
+            fr_elements.append(elt)
+    pd.DataFrame(fr_elements).to_json(f'{filename}_fr.jsonl', lines=True, orient='records')
+    #if args.get('extract_affiliations', False):
+    #    quote = '"'
+    #    cmd_1 = f"cd {volume} && cat {dump_file}.json | jq -rc '.attributes.contributors[].affiliation | @csv' | fgrep '{quote}' > {dump_file}_affiliations_contributors.jsonl"
+    #    logger.debug(cmd_1)
+    #    os.system(cmd_1)
+    #    cmd_2 = f"cd {volume} && cat {dump_file}.json | jq -rc '.attributes.creators[].affiliation | @csv' | fgrep '{quote}' > {dump_file}_affiliations_creators.jsonl"
+    #    logger.debug(cmd_2)
+    #    os.system(cmd_2)
+    #    cmd_11 = f'cd {volume} && cat {dump_file}_affiliations_contributors.jsonl | sort -u > {dump_file}_affiliations_contributors_uniq.jsonl'
+    #    cmd_21 = f'cd {volume} && cat {dump_file}_affiliations_creators.jsonl | sort -u > {dump_file}_affiliations_creators_uniq.jsonl'
+    #    logger.debug(cmd_11)
+    #    os.system(cmd_11)
+    #    logger.debug(cmd_21)
+    #    os.system(cmd_21)
+    #    cmd_3 = f'cd {volume} && cat {dump_file}_affiliations_contributors_uniq.jsonl > {dump_file}_affiliations.jsonl'
+    #    cmd_3 += f' && cat {dump_file}_affiliations_creators_uniq.jsonl >> {dump_file}_affiliations.jsonl'
+    #    cmd_3 += f' && cat {dump_file}_affiliations.jsonl | sort -u > {dump_file}_affiliations_uniq.jsonl'
+    #    logger.debug(cmd_3)
+    #    os.system(cmd_3)
+    #if args.get('match_affiliations', False):
+    #    os.system(f'mkdir -p {volume}/match')
+    #    chunk_res = []
+    #    ix = 0
+    #    with open(f'{volume}/{dump_file}_affiliations_creators_uniq.jsonl') as infile:
+    #        for line in infile:
+    #            query = line.replace('"', ' ').strip()
+    #            for match_type in ['country']:
+    #                elt = {"query": query, "type": match_type}
+    #                res = requests.post('http://matcher-affiliation:5001/match_api', json=elt).json()
+    #                elt['results'] = res['results']
+    #                if len(chunk_res) == 1000:
+    #                    logger.debug(f'write file nb {ix}')
+    #                    pd.DataFrame(chunk_res).to_json(f'{volume}/match/match_{ix}.jsonl', lines=True, orient='records')
+    #                    chunk_res = []
+    #                    ix += 1
+    #                chunk_res.append(elt)
 
 def create_task_analyze(args):
     for fileType in args.get('fileType', []):
